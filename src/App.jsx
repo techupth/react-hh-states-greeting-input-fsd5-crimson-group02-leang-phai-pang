370f284c6a9f541916bc21857b1d91cdb44667d1
devsomsak <devrleang@gmail.com>
2023-12-21 12:56:55 +0700

    feat:react-state-greeting-input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingTextInput,setGreetingTextInput]=useState("");
+  const [greetingText, setGreetingText] = useState("");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input 
+        id="greeting-message" 
+        type="text"
+        onChange={(event)=>{
+          setGreetingTextInput(event.target.value)
+        }}
+        value={greetingTextInput} />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+        onClick={()=>{
+          setGreetingText(greetingTextInput);
+          setGreetingTextInput("");
+        }}>Update text</button>
       </div>
     </div>
   );
